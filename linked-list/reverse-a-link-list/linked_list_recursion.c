dc56cdc1a72f74ff083d669ea7e7641989a02607
Amila619 <amilasandeepa349@gmail.com>
2025-05-28 19:42:44 +0530

    - printing elements of a linked list forward and reverse using recursion

---
+++ linked-list/reverse-a-link-list/linked_list_recursion.c
@@ -0,0 +1,69 @@
+// Linked List: Inserting a Node at beginning
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head; // global variable, can be accessed everywhere
+
+void Insert(int x)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+
+    if (head == NULL)
+    {
+        temp->next = head;
+        head = temp; // insertion at the begin
+    }
+    else
+    {
+        struct Node* temp1 = head;
+        while (temp1->next != NULL) // used to find the last node in the linked list
+        {
+            temp1 = temp1->next;
+        }
+        
+        temp1->next = temp; // Insertion at the end
+    }
+    
+};
+
+void Print(struct Node* temp)
+{
+    if(temp == NULL){
+        printf("\n");
+        return;
+    }
+    printf(" %d", temp->data);
+    Print(temp->next);
+};
+
+void RecurPrint(struct Node* temp)
+{
+    if(temp == NULL){
+        printf("\n");
+        return;
+    }
+    RecurPrint(temp->next);
+    printf(" %d", temp->data);
+};
+
+int main()
+{
+
+    head = NULL; // empty list
+    
+    Insert(1);
+    Insert(2);
+    Insert(3);
+    Insert(4);
+    Print(head);
+    RecurPrint(head);
+    return 0;
+}
